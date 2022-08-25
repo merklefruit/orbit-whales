84406826102546fb817a98ab86b2a6ba9b1deb86
Nicolas <nicolas@racchi.com>
2022-08-25 14:34:13 +0200

    feat: visual upgrades

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,47 +29,169 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Orbit <a href="#">Whales</a>
-        </h1>
+      {protocolTVL && topUsers && topPositions ? (
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Orbit{" "}
+            <a
+              href="https://development.orbitdefi.finance/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Whales
+            </a>
+          </h1>
 
-        <div style={{ marginTop: "1rem" }}>
-          TVL: {protocolTVL ?? "fetching..."}
-        </div>
+          <div style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+            <h2 style={{ fontWeight: "700", fontSize: "1.4rem" }}>
+              TVL: {protocolTVL} USD
+            </h2>
+          </div>
 
-        <div style={{ marginTop: "1rem" }}>
-          <h2 style={{ textAlign: "center" }}>Top 10 users</h2>
-          {topUsers ? (
-            <div>
+          <h2
+            style={{
+              margin: "0 32rem -0.7rem 0",
+              fontSize: "1.2rem",
+              backgroundColor: "white",
+              padding: "0 0.3rem 0 0.1rem",
+              zIndex: 1,
+            }}
+          >
+            Top 10 users
+          </h2>
+          <div
+            style={{
+              borderRadius: "0.6rem",
+              border: "solid black 2px",
+              padding: "1rem",
+            }}
+          >
+            <div style={{ width: "650px" }}>
               {topUsers.map((pm: any, idx: number) => (
-                <div key={idx}>
-                  {idx + 1} - {pm?.user} -{" "}
-                  <b>$ {+(+pm?.totalValueLocked).toFixed(2)}</b>
-                </div>
+                <a
+                  key={idx}
+                  href={`https://polygonscan.com/address/${pm.user}`}
+                  className="position-link"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  style={{
+                    textAlign: "left",
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    gap: "1.5rem",
+                    border: "1px solid lightgray",
+                    borderRadius: "0.5rem",
+                    padding: "0.5rem",
+                    marginBottom: "0.5rem",
+                    transition: "all 0.2s ease-in-out",
+                    cursor: "pointer",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "start",
+                      gap: "1.5rem",
+                    }}
+                  >
+                    <span>
+                      <span style={{ fontWeight: "700" }}>RANK: </span>
+                      {idx + 1}
+                    </span>
+                    <span>
+                      <span style={{ fontWeight: "700" }}>USER: </span>
+                      {pm.user.slice(0, 6)}...{pm.user.slice(-6)}
+                    </span>
+                  </div>
+                  <span style={{ width: "160px" }}>
+                    <span style={{ fontWeight: "700" }}>VALUE: </span> ${" "}
+                    {Number(pm.totalValueLocked).toFixed(2)}
+                  </span>
+                </a>
               ))}
             </div>
-          ) : (
-            "fetching..."
-          )}
-        </div>
+          </div>
 
-        <div style={{ marginTop: "1rem" }}>
-          <h2 style={{ textAlign: "center" }}>Top 10 positions</h2>
-          {topPositions ? (
-            <div>
+          <div style={{ marginTop: "1rem", marginBottom: "1rem" }} />
+
+          <h2
+            style={{
+              margin: "0 30rem -0.7rem 0",
+              fontSize: "1.2rem",
+              backgroundColor: "white",
+              padding: "0 0.3rem 0 0.1rem",
+              zIndex: 1,
+            }}
+          >
+            Top 10 positions
+          </h2>
+          <div
+            style={{
+              borderRadius: "0.6rem",
+              border: "solid black 2px",
+              padding: "1rem",
+            }}
+          >
+            <div style={{ width: "650px", paddingTop: "0.5rem" }}>
               {topPositions.map((pos: any, idx: number) => (
-                <div key={idx}>
-                  {idx + 1} - {pos?.id} - {pos.poolName} -
-                  <b>$ {+(+pos?.marketValueUSD).toFixed(2)}</b>
-                </div>
+                <a
+                  key={idx}
+                  href={`https://app.uniswap.org/#/pool/${pos?.id}?chain=polygon`}
+                  className="position-link"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                  style={{
+                    textAlign: "left",
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    gap: "1.5rem",
+                    border: "1px solid lightgray",
+                    borderRadius: "0.5rem",
+                    padding: "0.5rem",
+                    marginBottom: "0.5rem",
+                    transition: "all 0.2s ease-in-out",
+                    cursor: "pointer",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "start",
+                      gap: "1.5rem",
+                    }}
+                  >
+                    <span>
+                      <span style={{ fontWeight: "700" }}>RANK: </span>
+                      {idx + 1}
+                    </span>
+                    <span>
+                      <span style={{ fontWeight: "700" }}>ID: </span> {pos?.id}
+                    </span>
+                    <span>
+                      <span style={{ fontWeight: "700" }}>POOL: </span>{" "}
+                      {pos.poolName}
+                    </span>
+                  </div>
+                  <span style={{ width: "160px" }}>
+                    <span style={{ fontWeight: "700" }}>VALUE: </span> ${" "}
+                    {+(+pos?.marketValueUSD).toFixed(2)}
+                  </span>
+                </a>
               ))}
             </div>
-          ) : (
-            "fetching..."
-          )}
-        </div>
-      </main>
+          </div>
+        </main>
+      ) : (
+        <main className={styles.main}>
+          <h1 className={styles.title}>Fetching...</h1>
+        </main>
+      )}
     </div>
   );
 };
